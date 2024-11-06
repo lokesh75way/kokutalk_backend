ff469d4650536fbc7382f0aff766eb261ee6fc3b
Lokesh Bansal <lokeshbansal@Lokeshs-Mac-mini.local>
2024-11-06 12:13:05 +0530

    change for rate limiter

--- app/middleware/rateLimiter.ts
+++ app/middleware/rateLimiter.ts
@@ -7,18 +7,8 @@ import { IUser } from "../schema/User";
 
 loadConfig();
 
-const windowMs = parseInt(process.env.RATE_LIMIT_SECONDS || "2", 10);
-const max = parseInt(process.env.RATE_LIMIT_MAX_REQ || "10", 10);
-
-const blockedIps = new Set();
-
-const loadBlockedIps = async () => {
-    const ips = await BlockedIp.find({ isBlocked: true }, { ip: true }).lean()
-    ips.forEach(ip => { blockedIps.add(ip.ip) })
-    console.log("Blocked ips loaded,", blockedIps.size)
-}
-
-loadBlockedIps()
+const windowMs = parseInt(process.env.RATE_LIMIT_MINUTES || "5", 10);
+const max = parseInt(process.env.RATE_LIMIT_MAX_REQ || "20", 10);
 
 const getIp = (req: Request) => {
     let ip = req.headers['x-forwarded-for'] as string || req.socket["remoteAddress"]?.split(",")[0] || req.ip || "";
@@ -37,38 +27,30 @@ const getIp = (req: Request) => {
     return ip;
 }
 
-const blocker = (req: Request, res: Response, next: NextFunction) => {
-    const clientIp = getIp(req);
-    if (blockedIps.has(clientIp)) {
-        return res.status(403).send('Your IP has been permanently blocked.');
-    }
-    next();
-}
-
 // Configure the rate limit
 const limiter = rateLimit({
-    windowMs: windowMs * 1000,
-    max: max,
+    windowMs: windowMs * 60 * 1000,
+    max: max * windowMs,
     standardHeaders: true,
     legacyHeaders: false,
     handler: (req, res) => {
         try {
-            const clientIp = getIp(req);
-            blockedIps.add(clientIp);
-            BlockedIp.updateOne({ ip: clientIp }, { isBlocked: true }, { upsert: true }).then(console.log).catch(console.log)
-            res.status(429).send('Too many requests. You have been permanently blocked.');
+            res.status(429).send('Too many requests. You have been blocked. Please try after 5 minutes.');
         } catch (error) {
-            res.status(429).send('Too many requests. You have been permanently blocked.');
+            res.status(429).send('Too many requests. You have been permanently blocked. Please try after 5 minutes.');
         }
         return;
     },
     skip: async (req: Request, res: Response) => {
-        const rateLimitedApi = ["register", "login", "google", "facebook"]
+        const rateLimitedApi = ["send-otp", "verify-otp", "google", "facebook"]
         const url = req?.originalUrl?.split("?")[0];
         const apiRateLimited = rateLimitedApi.filter((curr) => url.includes(curr));
         return apiRateLimited?.length == 0;
 
+    },
+    keyGenerator: (req: Request, res: Response) => {
+        return getIp(req)
     }
 });
 
-export const apiLimiter = [blocker, limiter];
+export const apiLimiter = [limiter];
